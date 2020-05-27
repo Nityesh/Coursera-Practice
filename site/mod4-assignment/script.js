4fdca30e8a79e3acac05a68b7ce802cf22ba8175
Nityesh <nityeshmadipalle@gmail.com>
2020-05-27 15:15:10 +0530

    Mod4 Assignment

--- site/mod4-assignment/script.js
+++ site/mod4-assignment/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Nityesh", "Teja", "Vani", "John", "Jahnavi", "Akhil", "Geeth", "Jim", "Asritha", "James"];
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
   if (firstLetter === 'j') {
